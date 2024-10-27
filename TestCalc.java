b224f32161b4f051ab9b84e8badee02c65c405a8
vtaboranskyi <tab.vov38@gmail.com>
2024-10-27 12:50:10 +0200

    first commit

---
+++ TestCalc.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class TestCalc {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+        System.out.println(Math.pow(-5, -2));
+        System.out.println("Hello!");
+    }
+}
